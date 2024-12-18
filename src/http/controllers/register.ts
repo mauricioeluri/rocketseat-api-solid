f9106edf214074f515e054e0d414341d68f58255
Maurício El Uri <mauriciom.eluri@gmail.com>
2024-12-18 15:25:15 -0300

    Starter application - user creation

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,31 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
+import { prisma } from '@/lib/prisma'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  const password_hash = await hash(password, 6)
+
+  const userWithSomeEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSomeEmail)
+    return reply.status(409).send({ error: 'User already exists' })
+
+  await prisma.user.create({
+    data: { name, email, password_hash },
+  })
+
+  return reply.status(201).send()
+}

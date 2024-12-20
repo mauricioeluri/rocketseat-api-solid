0f2a33edc2c33c45b217afc2f711b70f6f4fe95f
Maurício El Uri <mauriciom.eluri@gmail.com>
2024-12-20 02:25:17 -0300

    auth use case

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -5,6 +5,19 @@ import { RegisterUseCase } from './register'
 import { UserAlreadyExistsError } from './errors/user-alread-exists-error'
 
 describe('Register Use Case', () => {
+  it('should be able to register', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456'
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
   it('should hash user password upon registration', async () => {
     const usersRepository = new InMemoryUsersRepository()
     const registerUseCase = new RegisterUseCase(usersRepository)
@@ -32,25 +45,12 @@ describe('Register Use Case', () => {
       password: '123456'
     })
 
-    expect(() =>
+    await expect(() =>
       registerUseCase.execute({
         name: 'John Doe',
         email,
         password: '123456'
       }),
     ).rejects.toBeInstanceOf(UserAlreadyExistsError)
-  })
-
-  it('should be able to register', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
-
-    const { user } = await registerUseCase.execute({
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password: '123456'
-    })
-
-    expect(user.id).toEqual(expect.any(String))
   })
 })

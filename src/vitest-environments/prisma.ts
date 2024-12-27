d70a4b8d1f6a4aa21f4132f3e30697411d37242f
Maurício El Uri <mauriciom.eluri@gmail.com>
2024-12-27 11:49:01 -0300

    Config prisma env to mysql e2e test

--- src/vitest-environments/prisma.ts
+++ src/vitest-environments/prisma.ts
@@ -1,14 +1,36 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 import { Environment } from 'vitest/environments'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseUrl(schema: string) {
+  if (
+    !process.env.MYSQL_USER ||
+    !process.env.MYSQL_PASSWORD ||
+    !process.env.MYSQL_HOST ||
+    !process.env.MYSQL_PORT
+  )
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+
+  return `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT}/${schema}`
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseUrl(schema)
+    process.env.DATABASE_URL = databaseURL
+    execSync('pnpm prisma migrate deploy')
 
     return {
-      teardown() {
-        console.log('Teardown')
+      async teardown() {
+        await prisma.$executeRawUnsafe(`DROP DATABASE IF EXISTS \`${schema}\`;`)
+        await prisma.$disconnect()
       },
     }
   },

36616543b71c458872f0e6f0dd77d4c2487e7009
Maurício El Uri <mauriciom.eluri@gmail.com>
2024-12-27 12:49:22 -0300

    test e2e profile

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -6,7 +6,9 @@ export async function profile(request: FastifyRequest, reply: FastifyReply) {
   const getUserProfile = makeGetUserProfileUseCase()
   const { user } = await getUserProfile.execute({ userId: request.user.sub })
   return reply.status(200).send({
-    ...user,
-    password_hash: undefined,
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
   })
 }

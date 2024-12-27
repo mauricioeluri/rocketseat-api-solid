ea148ac6bb67c8486c5c4001e73fec8ddc5f8e7b
Maurício El Uri <mauriciom.eluri@gmail.com>
2024-12-27 12:29:10 -0300

    First e2e test - register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,4 +1,22 @@
-import { test, expect } from 'vitest'
-test('ok', () => {
-  expect(1).toBe(1)
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Register (e2e)', () => {
+  beforeAll(() => {
+    app.ready()
+  })
+
+  afterAll(() => {
+    app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+    expect(response.statusCode).toEqual(201)
+  })
 })

373a6d5eb28b7853bb68dda627130434fe72469c
Maurício El Uri <mauriciom.eluri@gmail.com>
2024-12-27 13:03:50 -0300

    Gym controller and new organization of routes

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,16 @@ import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
-import { appRoutes } from './http/routes'
+import { gymsRoutes } from './http/controllers/gyms/routes'
+import { userRoutes } from './http/controllers/users/routes'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
-app.register(appRoutes)
+app.register(userRoutes)
+app.register(gymsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError)

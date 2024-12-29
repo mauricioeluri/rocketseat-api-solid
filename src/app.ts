884b772004726462db7bace0954e56c01e5b426e
Maurício El Uri <mauriciom.eluri@gmail.com>
2024-12-29 03:38:06 -0300

    Setting refresh token cookie

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,25 @@
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
 import { userRoutes } from './http/controllers/users/routes'
-import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: { expiresIn: '10m' },
 })
+
+app.register(fastifyCookie)
+
 app.register(userRoutes)
 app.register(gymsRoutes)
 app.register(checkInsRoutes)

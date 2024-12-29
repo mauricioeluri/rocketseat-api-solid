12dc41371627db32e899bb3c95f790f17e9c911a
Maurício El Uri <mauriciom.eluri@gmail.com>
2024-12-29 02:08:50 -0300

    check-ins routes and controllers

---
+++ src/http/controllers/check-ins/create.ts
@@ -0,0 +1,30 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeCheckInUseCase } from '@/use-cases/fatories/make-check-in-use-case'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createCheckInsParamsSchema = z.object({
+    gymId: z.string().uuid(),
+  })
+  const createCheckInBodySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+  })
+
+  const { gymId } = createCheckInsParamsSchema.parse(request.params)
+  const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
+
+  const createGymUseCase = makeCheckInUseCase()
+
+  await createGymUseCase.execute({
+    gymId,
+    userId: request.user.sub,
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+  return reply.status(201).send()
+}
--- src/use-cases/fatories/make-get-user-metrics-use-case.ts
+++ src/use-cases/fatories/make-get-user-metrics-use-case.ts
@@ -1,7 +1,7 @@
 import { GetUserMetricsUseCase } from '../get-user-metrics'
 import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
 
-export function makeUserMetricsUseCase() {
+export function makeGetUserMetricsUseCase() {
   const checkInsRepository = new PrismaCheckInsRepository()
   return new GetUserMetricsUseCase(checkInsRepository)
 }
